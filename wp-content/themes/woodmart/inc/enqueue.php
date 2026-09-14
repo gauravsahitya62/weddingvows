8fc6ad1837ad8aa12eb8026f303fe8c3317efd53
gauravsahitya62 <gauravsahitya62@gmail.com>
2026-09-14 21:53:31 +0530

    Load responsive money-page and mobile navigation styles

--- wp-content/themes/woodmart/inc/enqueue.php
+++ wp-content/themes/woodmart/inc/enqueue.php
@@ -6,9 +6,10 @@ function theme_files() {
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css', array(), '6.5.2');
     wp_enqueue_style('icons', get_theme_file_uri('/css/icons.min.css'), array(), null);
     wp_enqueue_style('main_styles', get_theme_file_uri('/css/app.min.css'), array(), '1.2.2');
-    wp_enqueue_style('wvn-bts', get_theme_file_uri('/css/bts.css'), array(), '1.8.27');
+    wp_enqueue_style('wvn-bts', get_theme_file_uri('/css/bts.css'), array('main_styles'), '1.8.27');
     wp_enqueue_style('wvn-overrides', get_theme_file_uri('/css/wvn-overrides.css'), array('wvn-bts'), '1.0.0');
     wp_enqueue_style('wvn-seo-growth', get_theme_file_uri('/css/wvn-seo-growth.css'), array('wvn-overrides'), '1.0.0');
+    wp_enqueue_style('wvn-money-responsive', get_theme_file_uri('/css/wvn-money-responsive.css'), array('wvn-seo-growth'), '1.0.0');
     wp_enqueue_style('custom-swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11');
     wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), '11', true);
     wp_enqueue_script('jqueryui', 'https://code.jquery.com/jquery-3.6.3.min.js', array('jquery'), '3.6.3', true);
