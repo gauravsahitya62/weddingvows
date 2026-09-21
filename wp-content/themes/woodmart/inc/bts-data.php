894fa3c9e3e97dfeaddbd901aefe8429a123a8d8
gauravsahitya62 <gauravsahitya62@gmail.com>
2026-09-21 13:43:30 +0530

    Wrap testimonial quotes for dynamic scrolling

--- wp-content/themes/woodmart/inc/bts-data.php
+++ wp-content/themes/woodmart/inc/bts-data.php
@@ -1294,9 +1294,11 @@ function wvn_press_leaf($leaf) {
         <?php if (!empty($leaf['title'])) : ?><h3><?php echo esc_html($leaf['title']); ?></h3><?php endif; ?>
         <?php if (!empty($leaf['brand'])) : ?><p class="wvn-pressbook-brand"><?php echo esc_html($leaf['brand']); ?></p><?php endif; ?>
         <?php if (!empty($leaf['text'])) : ?>
-          <?php foreach (preg_split('/\n\s*\n/', trim($leaf['text'])) as $para) : ?>
-            <p><?php echo nl2br(esc_html(trim($para))); ?></p>
-          <?php endforeach; ?>
+          <div class="wvn-pressbook-quote-scroll">
+            <?php foreach (preg_split('/\n\s*\n/', trim($leaf['text'])) as $para) : ?>
+              <p><?php echo nl2br(esc_html(trim($para))); ?></p>
+            <?php endforeach; ?>
+          </div>
         <?php endif; ?>
       <?php else : ?>
         <?php if (!empty($leaf['brand'])) : ?><p class="wvn-pressbook-brand"><?php echo esc_html($leaf['brand']); ?></p><?php endif; ?>
